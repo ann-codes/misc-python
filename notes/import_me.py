7faf33ccfcaa5a8c4d4307d4b96212c6b7cc56e0
Ann Nguyen <annfcz@gmail.com>
2020-07-06 03:03:30 +0000

    add notes on importing scripts

---
+++ notes/import_me.py
@@ -0,0 +1,21 @@
+print("Printing from the imported script!")
+
+num = 91873891273
+
+def add_two(num):
+  return num + 2
+
+def main():
+    print("Testing mean function")
+    n_list = [34, 44, 23, 46, 12, 24]
+    correct_mean = 30.5
+    assert(mean(n_list) == correct_mean)
+
+    print("Testing add_five function")
+    correct_list = [39, 49, 28, 51, 17, 29]
+    assert(add_five(n_list) == correct_list)
+
+    print("All tests passed!")
+
+if __name__ == '__main__':
+    main()
